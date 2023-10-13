eda5f2d0e7b47228de78c5cd6ba80e219a633d10
wuchanye <147586984+wuchanye@users.noreply.github.com>
2023-10-13 19:51:24 +0800

    Update app.py

--- app.py
+++ app.py
@@ -37,143 +37,165 @@ def callback():
         abort(400)
     return 'OK'
 
+WAITING_FOR_FOOD_NAME = 1
+WAITING_FOR_QUERY_NUM = 2
+WAITING_FOR_IMG = 3
+NORMAL = 0
+current_state = None
+search_results = []
+
+@handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     user_message = event.message.text
-    keyword = user_message
+    
+    global current_state
+    global keyword
+    global search_results
+    global img_path
+    
+    if current_state == WAITING_FOR_FOOD_NAME:
+        keyword = user_message  # 將用戶的回覆儲存在 keyword 變數中
+        current_state = NORMAL  # 回復正常狀態
+        search_results, messages = search(keyword)
+        
+        for i in range(len(messages) - 1, 0, -1):
+            messages[i] = messages[i - 1]
+        messages[0] = title
+        messages_text = "\n".join(messages)
+        
+        message = TextSendMessage(messages_text)
+        line_bot_api.reply_message(event.reply_token, message)
+
+        filename = save_file(messages_text)
+        github_repo = 'wuchanye/test'
+        github_path = 'imgs2/' + img_keyword + '.jpg'
+        github_token = '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'
+        upload_image_to_github(filename, github_repo, github_path, github_token)
+    else:
+        if user_message == ("@查詢"):
+            current_state = WAITING_FOR_FOOD_NAME
+            message = TextSendMessage(text='請輸入食物名稱：')
+            line_bot_api.reply_message(event.reply_token, message)
+            
+
+#搜尋食物 獲取食物名稱,foodId
+def search(keyword):
     messages = []
+    m=1
+    
+    # 繁體中文關鍵字
+    encoded_keyword_traditional = parse.quote(keyword)
+    t2s = OpenCC('t2s') #繁轉簡
+    s2t = OpenCC('s2t') #簡轉繁
+    simplified_name = t2s.convert(keyword)
+    # 簡體中文關鍵字
+    encoded_keyword_simplified = parse.quote(simplified_name)
+    
+    data_simplified = {} #解決繁簡相同時 未賦予值的問題
 
-    
-    img_url, message_text = download_images_and_upload_to_github(1, keyword, github_token)
-    messages_text = "\n".join(messages_text)
-    #img_message = ImageSendMessage(original_content_url=f'{img_url}',  preview_image_url=f'{img_url}')
-    #messages = [messages_text , img_message]
-    
-    line_bot_api.reply_message(event.reply_token, messages_text)
-   
+    while True:
+        # 繁簡相異
+        if encoded_keyword_traditional != encoded_keyword_simplified:
+            # app_id=uxnxyhgkujxpgqbt&app_secret=UHJqRjR6S25DaSttRWxZUElSbnovUT09
+            # 搜尋繁體中文資料
+            url_traditional = f"https://www.mxnzp.com/api/food_heat/food/search?keyword={encoded_keyword_traditional}&page={m}&app_id=uxnxyhgkujxpgqbt&app_secret=UHJqRjR6S25DaSttRWxZUElSbnovUT09"
+            with urllib_request.urlopen(url_traditional) as response_traditional:
+                data_traditional = json.load(response_traditional)
 
-def init_browser(keyword):
-    url = f'https://www.google.com.hk/search?q={img_keyword}&tbm=isch'
-    
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument("--disable-infobars") # 禁用一些瀏覽器的信息欄
-    
-    # 創建一個帶有特定配置的 Chrome 瀏覽器實例
-    browser = webdriver.Chrome(options=chrome_options)
-    
-    browser.get(url)
-    browser.maximize_window()
-    
-    return browserdef init_browser(img_keyword):
-    url = f'https://www.google.com.hk/search?q={img_keyword}&tbm=isch'
-    
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument("--disable-infobars") # 禁用一些瀏覽器的信息欄
-    
-    # 創建一個帶有特定配置的 Chrome 瀏覽器實例
-    browser = webdriver.Chrome(options=chrome_options)
-    
-    browser.get(url)
-    browser.maximize_window()
-    
-    return browser
+            time.sleep(0.3)
+            # 搜尋簡體中文資料
+            url_simplified = f"https://www.mxnzp.com/api/food_heat/food/search?keyword={encoded_keyword_simplified}&page={m}&app_id=uxnxyhgkujxpgqbt&app_secret=UHJqRjR6S25DaSttRWxZUElSbnovUT09"
+            with urllib_request.urlopen(url_simplified) as response_simplified:
+                data_simplified = json.load(response_simplified)
 
-def download_images_and_upload_to_github(round, keyword, github_token):
-    message_text = []
-    # Initialize the browser
-    browser = init_browser(keyword)
+            if ('data' in data_traditional and 'list' in data_traditional['data']) or ('data' in data_simplified and 'list' in data_simplified['data']):
+                for i in range(10):
+                    if i < len(data_traditional['data']['list']):
+                        # all(): 檢查迭代器中的所有元素是否都為真，當所有元素都為真時，函式會返回 True，否則返回 False，通常用於需要檢查多個條件的情況
+                        if all(item['name'] != data_traditional['data']['list'][i]['name'] for item in search_results):
+                            search_results.append(data_traditional['data']['list'][i])
+                            messages.append(f"{len(search_results)}. {search_results[-1]['name']}")
+                            if len(search_results) == 10:
+                                break
+                    if i < len(data_simplified['data']['list']):
+                        traditional_name = s2t.convert(data_simplified['data']['list'][i]['name'])
+                        if all(item['name'] != traditional_name for item in search_results):
+                            data_simplified['data']['list'][i]['name'] = traditional_name
+                            search_results.append(data_simplified['data']['list'][i])
+                            messages.append(f"{len(search_results)}. {search_results[-1]['name']}")
+                            if len(search_results) == 10:
+                                break
+        # 繁簡體相同
+        else:
+            url_traditional = f"https://www.mxnzp.com/api/food_heat/food/search?keyword={encoded_keyword_traditional}&page={m}&app_id=uxnxyhgkujxpgqbt&app_secret=UHJqRjR6S25DaSttRWxZUElSbnovUT09"
+            with urllib_request.urlopen(url_traditional) as response_traditional:
+                data_traditional = json.load(response_traditional)
+
+            if 'data' in data_traditional and 'list' in data_traditional['data']:
+                for i in range(10):
+                    if i < len(data_traditional['data']['list']):
+                        traditional_name = s2t.convert(data_traditional['data']['list'][i]['name'])
+                        if all(item['name'] != traditional_name for item in search_results):
+                            data_traditional['data']['list'][i]['name'] = traditional_name
+                            search_results.append(data_traditional['data']['list'][i])
+                            messages.append(f"{len(search_results)}. {search_results[-1]['name']}")
+                            if len(search_results) == 10:
+                                break
+        if len(search_results) == 0:
+            messages.append("目前還沒有相關的食物資料！！")
+            break
+        elif len(search_results) < 10:
+            # 若 page = m 跑完 search_results 不滿 10 筆資料, 且兩種陣列有任一個是第一頁有十筆資料的，再跑下一頁 (m+=1)
+            if ('data' in data_traditional and 'list' in data_traditional['data']) and i == len(data_traditional['data']['list']):
+                m += 1
+                time.sleep(0.3)
+            elif ('data' in data_simplified and 'list' in data_simplified['data']) and i == len(data_simplified['data']['list']):
+                m += 1
+                time.sleep(0.3)
+            else:
+                break
+        else:
+            break
+        
+    return search_results, messages
+
+def save_file(messages_text):
     local_folder = 'imgs2'
-    
     if not os.path.exists(local_folder):
         os.makedirs(local_folder)
         
-    data_url_count = 0
-    max_image_size = 1024 * 1024
-    github_repo = 'wuchanye/test'  # Replace with your GitHub repository
-    github_path = 'imgs2/' + img_keyword + '.jpg'
+    filename = f'./{local_folder}/mytext.txt'
+    with open(fil_name, "w", encoding="utf-8") as file:
+        # 写入文本内容到文件
+        file.write(messages_text)
+    return filename
     
-    for i in range(round):
-        try:
-            parent_element = WebDriverWait(browser, 20).until(
-                EC.presence_of_element_located((By.CSS_SELECTOR, '.islrc')))
-            
-            img_elements = WebDriverWait(parent_element, 20).until(
-                EC.presence_of_all_elements_located((By.TAG_NAME, 'img')))
-            
-            for element in img_elements:
-                img_url = element.get_attribute('src')
-                
-                img_attributes = element.get_attribute('outerHTML')
-                
-                if isinstance(img_url, str):
-                    if img_url.startswith('data:image/jpeg'):
-                        if 'data-sz' not in img_attributes:
-                            if data_url_count == 0:
-                                data_url_count += 1
-                                continue
-                            else:
-                                img_data = re.search(r'base64,(.*)', img_url).group(1)
-                                image_data = base64.b64decode(img_data)
-                                
-                                if len(image_data) < max_image_size:
-                                    image = Image.open(BytesIO(image_data))
-                                    image = image.convert("RGB")
-                                    
-                                    # Save the image locally
-                                    local_filename = f'./{local_folder}/{img_keyword}.jpg'
-                                    image.save(local_filename)
-                                    message_text.append('save file done')
-                                    
-                                    # Upload the image to GitHub
-                                    message_text = upload_image_to_github(local_filename, github_repo, github_path, github_token, message_text)
-                                    img_url = f'https://raw.githubusercontent.com/{github_repo}/main/img2/{img_keyword}.jpg'
-                                    return img_url, message_text
-                                    break
-                    else:
-                        if len(img_url) <= 200:
-                            if 'images' in img_url:
-                                local_filename = f'./{local_folder}/{img_keyword}.jpg'
-                                r = requests.get(img_url)
-                                if len(r.content) < max_image_size:
-                                    with open(local_filename, 'wb') as file:
-                                        file.write(r.content)
-                                        file.close()
-                                        message_text = upload_image_to_github(local_filename, github_repo, github_path, github_token)
-                                        img_url = f'https://raw.githubusercontent.com/{github_repo}/main/img2/{img_keyword}.jpg'
-                                        return img_url, message_text
-                                        break
-        except StaleElementReferenceException:
-            print("出現StaleElementReferenceException錯誤，重新定位元素")
-            continue
-        
-    time.sleep(0.5)
-    browser.close()
-    print("爬取完成") 
-
-def upload_image_to_github(local_filename, github_repo, github_path, github_token, message_text):
-    with open(local_filename, 'rb') as file:
+def upload_image_to_github(filename, github_repo, github_path, github_token):
+    with open(filename, 'rb') as file:
         content = file.read()
-
     headers = {
         "Authorization": f"Bearer {github_token}",
         "Content-Type": "application/json",
     }
-    
     url = f"https://api.github.com/repos/{github_repo}/contents/{github_path}"
+    
     data = {
         "message": "Add image",
         "content": base64.b64encode(content).decode('utf-8'),
     }
-
     response = requests.put(url, headers=headers, json=data)
+    #content = response.json()
+    
+    #image_url = content.get('download_url')
+    #img_message = ImageSendMessage(
+    #original_content_url=f'{image_url}',  
+    #preview_image_url=f'{image_url}'  
+    #)
+    #return img_message
 
-    if response.status_code == 200:
-        message_text.append("Image uploaded to GitHub successfully.")
-    else:
-        message_text.append("Failed to upload image to GitHub.")
-        message_text.append(response.text)
-    return message_text
 
-  # 主程式
+# 主程式
 if __name__ == '__main__':
+    current_state = NORMAL
     keyword = ""
     app.run()
