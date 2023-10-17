0f156469814265d340f4b705e15792af3b34fd7c
wuchanye <147586984+wuchanye@users.noreply.github.com>
2023-10-17 13:28:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,23 +2,12 @@ from flask import request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage, ImageSendMessage
-from opencc import OpenCC
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import StaleElementReferenceException
-from io import BytesIO
-from PIL import Image
-import pyimgur
-import urllib.request as urllib_request
-import urllib.parse as parse
-import json
 import time
 import os
 import requests
-import re
 import base64
+from bs4 import BeautifulSoup
+
 
 from flask import Flask
 app = Flask(__name__)
@@ -42,7 +31,6 @@ WAITING_FOR_QUERY_NUM = 2
 WAITING_FOR_IMG = 3
 NORMAL = 0
 current_state = None
-search_results = []
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
@@ -50,153 +38,103 @@ def handle_message(event):
     
     global current_state
     global keyword
-    global search_results
-    global img_path
     
     if current_state == WAITING_FOR_FOOD_NAME:
         keyword = user_message  # 將用戶的回覆儲存在 keyword 變數中
         current_state = NORMAL  # 回復正常狀態
-        search_results, messages = search(keyword)
-
-        title = '請選擇想查詢的食物代號：\n'
-        for i in range(len(messages) - 1, 0, -1):
-            messages[i] = messages[i - 1]
-        messages[0] = title
-        messages_text = "\n".join(messages)
+        img_content = download_images_with_beautifulsoup(1, keyword)
         
-        message = TextSendMessage(messages_text)
-        line_bot_api.reply_message(event.reply_token, message)
-
-        filename = save_file(messages_text)
-        github_repo = 'wuchanye/download_to_github'
-        github_path = 'imgs2/'+keyword+'.jpg'
-        github_token = os.environ.get('github_token')
-        upload_image_to_github(filename, github_repo, github_path, github_token)
+        if img_content:  # 不是空列表，即有搜尋結果
+            github_token = os.environ.get('github_token')
+            img_message = upload_image_to_github(img_content, keyword, github_token)
+            line_bot_api.reply_message(event.reply_token, img_message)
     else:
         if user_message == ("@查詢"):
             current_state = WAITING_FOR_FOOD_NAME
             message = TextSendMessage(text='請輸入食物名稱：')
             line_bot_api.reply_message(event.reply_token, message)
+                    
+    
+def download_images_with_beautifulsoup(round, img_keyword):
+    # 獲取 html 內容
+    url = f'https://www.google.com.hk/search?q={img_keyword}&tbm=isch'
+    resp = requests.get(url)
+    soup = BeautifulSoup(resp.text, 'lxml')
+    
+    max_image_size = 1024 * 1024
+    img_content = None  # 用于存储图像内容
+    
+    for i in range(round):
+        try:
+            # 分析網頁, 找圖片元素
+            img_elements = soup.find_all('img') 
             
-
-#搜尋食物 獲取食物名稱,foodId
-def search(keyword):
-    messages = []
-    m=1
-    
-    # 繁體中文關鍵字
-    encoded_keyword_traditional = parse.quote(keyword)
-    t2s = OpenCC('t2s') #繁轉簡
-    s2t = OpenCC('s2t') #簡轉繁
-    simplified_name = t2s.convert(keyword)
-    # 簡體中文關鍵字
-    encoded_keyword_simplified = parse.quote(simplified_name)
-    
-    data_simplified = {} #解決繁簡相同時 未賦予值的問題
-
-    while True:
-        # 繁簡相異
-        if encoded_keyword_traditional != encoded_keyword_simplified:
-            # app_id=uxnxyhgkujxpgqbt&app_secret=UHJqRjR6S25DaSttRWxZUElSbnovUT09
-            # 搜尋繁體中文資料
-            url_traditional = f"https://www.mxnzp.com/api/food_heat/food/search?keyword={encoded_keyword_traditional}&page={m}&app_id=uxnxyhgkujxpgqbt&app_secret=UHJqRjR6S25DaSttRWxZUElSbnovUT09"
-            with urllib_request.urlopen(url_traditional) as response_traditional:
-                data_traditional = json.load(response_traditional)
-
-            time.sleep(0.3)
-            # 搜尋簡體中文資料
-            url_simplified = f"https://www.mxnzp.com/api/food_heat/food/search?keyword={encoded_keyword_simplified}&page={m}&app_id=uxnxyhgkujxpgqbt&app_secret=UHJqRjR6S25DaSttRWxZUElSbnovUT09"
-            with urllib_request.urlopen(url_simplified) as response_simplified:
-                data_simplified = json.load(response_simplified)
-
-            if ('data' in data_traditional and 'list' in data_traditional['data']) or ('data' in data_simplified and 'list' in data_simplified['data']):
-                for i in range(10):
-                    if i < len(data_traditional['data']['list']):
-                        # all(): 檢查迭代器中的所有元素是否都為真，當所有元素都為真時，函式會返回 True，否則返回 False，通常用於需要檢查多個條件的情況
-                        if all(item['name'] != data_traditional['data']['list'][i]['name'] for item in search_results):
-                            search_results.append(data_traditional['data']['list'][i])
-                            messages.append(f"{len(search_results)}. {search_results[-1]['name']}")
-                            if len(search_results) == 10:
-                                break
-                    if i < len(data_simplified['data']['list']):
-                        traditional_name = s2t.convert(data_simplified['data']['list'][i]['name'])
-                        if all(item['name'] != traditional_name for item in search_results):
-                            data_simplified['data']['list'][i]['name'] = traditional_name
-                            search_results.append(data_simplified['data']['list'][i])
-                            messages.append(f"{len(search_results)}. {search_results[-1]['name']}")
-                            if len(search_results) == 10:
-                                break
-        # 繁簡體相同
-        else:
-            url_traditional = f"https://www.mxnzp.com/api/food_heat/food/search?keyword={encoded_keyword_traditional}&page={m}&app_id=uxnxyhgkujxpgqbt&app_secret=UHJqRjR6S25DaSttRWxZUElSbnovUT09"
-            with urllib_request.urlopen(url_traditional) as response_traditional:
-                data_traditional = json.load(response_traditional)
-
-            if 'data' in data_traditional and 'list' in data_traditional['data']:
-                for i in range(10):
-                    if i < len(data_traditional['data']['list']):
-                        traditional_name = s2t.convert(data_traditional['data']['list'][i]['name'])
-                        if all(item['name'] != traditional_name for item in search_results):
-                            data_traditional['data']['list'][i]['name'] = traditional_name
-                            search_results.append(data_traditional['data']['list'][i])
-                            messages.append(f"{len(search_results)}. {search_results[-1]['name']}")
-                            if len(search_results) == 10:
-                                break
-        if len(search_results) == 0:
-            messages.append("目前還沒有相關的食物資料！！")
-            break
-        elif len(search_results) < 10:
-            # 若 page = m 跑完 search_results 不滿 10 筆資料, 且兩種陣列有任一個是第一頁有十筆資料的，再跑下一頁 (m+=1)
-            if ('data' in data_traditional and 'list' in data_traditional['data']) and i == len(data_traditional['data']['list']):
-                m += 1
-                time.sleep(0.3)
-            elif ('data' in data_simplified and 'list' in data_simplified['data']) and i == len(data_simplified['data']['list']):
-                m += 1
-                time.sleep(0.3)
-            else:
-                break
-        else:
-            break
+            for element in img_elements:
+                # 遍歷元素, 鎖定屬性
+                img_url = element.get('src')
+                if isinstance(img_url, str) and img_url.startswith('https://'): # 留下https://開頭的url
+                    r = requests.get(img_url)
+                    if len(r.content) < max_image_size: 
+                        img_content = r.content # 將二進制內容儲存在參數
+                        break
+        except Exception as e:
+            print(f"出现错误：{str(e)}")
+            continue
         
-    return search_results, messages
+    print("爬取完成")
+    time.sleep(0.5)
+    return img_content
 
-def save_file(messages_text):
-    local_folder = 'imgs2'
-    if not os.path.exists(local_folder):
-        os.makedirs(local_folder)
-        
-    filename = f'./{local_folder}/mytext.txt'
-    with open(filename, "w", encoding="utf-8") as file:
-        # 写入文本内容到文件
-        file.write(messages_text)
-    return filename
-    
-def upload_image_to_github(filename, github_repo, github_path, github_token):
-    with open(filename, 'rb') as file:
-        content = file.read()
-    headers = {
-        "Authorization": f"Bearer {github_token}",
-        "Content-Type": "application/json",
-    }
-    url = f"https://api.github.com/repos/{github_repo}/contents/{github_path}"
-    
-    data = {
-        "message": "Add image",
-        "content": base64.b64encode(content).decode('utf-8'),
-    }
-    response = requests.put(url, headers=headers, json=data)
-    #content = response.json()
-    
-    #image_url = content.get('download_url')
-    #img_message = ImageSendMessage(
-    #original_content_url=f'{image_url}',  
-    #preview_image_url=f'{image_url}'  
-    #)
-    #return img_message
 
+def upload_image_to_github(image_content, keyword, github_token):
+    github_username = 'wuchanye'
+    github_repo = 'download_to_github'
+    github_folder = 'img2'
+    filename = keyword + '.jpg'
+    
+    
+    # 獲取現有文件的 SHA 值
+    url_get_sha = f'https://api.github.com/repos/{github_username}/{github_repo}/contents/{github_folder}/{filename}'
+    headers_get_sha = {
+        'Authorization': f'Bearer {github_token}',
+        'Content-Type': 'application/json',
+    }
+    
+    response_get_sha = requests.get(url_get_sha, headers=headers_get_sha)
+    sha_data = response_get_sha.json()
+    sha_value = sha_data.get('sha')
+    
+    if sha_value:
+        url = f'https://api.github.com/repos/{github_username}/{github_repo}/contents/{github_folder}/{filename}'
+        headers = {
+            'Authorization': f'Bearer {github_token}',
+            'Content-Type': 'application/json',
+        }
+
+        data = {
+            'message': 'Update image',
+            'content': base64.b64encode(image_content).decode('utf-8'),
+            'sha': sha_value  # 傳遞 SHA 值
+        }
+
+        response = requests.put(url, headers=headers, json=data)
+    
+        if response.status_code == 200 or response.status_code == 201:
+            image_url = response.json().get('content').get('download_url')
+            img_message = ImageSendMessage(
+                original_content_url=image_url,
+                preview_image_url=image_url
+            )
+            print("文件已成功更新到GitHub存储库的文件夹。")
+        else:
+            print(f"上传文件失败，HTTP响应代码: {response.status_code}")
+            print(f"响应内容: {response.text}")
+            img_message = TextSendMessage(text="上傳圖片失敗。")
+    return img_message
 
 # 主程式
 if __name__ == '__main__':
     current_state = NORMAL
     keyword = ""
+    
     app.run()
